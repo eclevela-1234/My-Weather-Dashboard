4fd9320be0615a98dcbf74a6d6c495484bd4786c
Eliot Cleveland <eclevela@richmond.edu>
2022-07-03 11:13:59 -0400

    pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// place api key here for development
+
+// get coordinates for city
+
+var getCoords = function(city) {
+
+    api
+}
+
+// plug coords into one call api
+
+
+// display results in console
+
+
+// traverse response to disired info and store in object array
+
+
+// dynamically create elements and display on page
+
+
+// add persistence - store city names/coords
+
+// add auto load with current location info
