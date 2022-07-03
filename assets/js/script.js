60825fa89df8a8d97e6aeff526ec94bdc7667626
Eliot Cleveland <eclevela@richmond.edu>
2022-07-03 12:21:33 -0400

    both working apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,39 @@
 // place api key here for development
-
+var apiKey = "18e2ac046083c4c6e61f1e447f65bee7"
 // get coordinates for city
 
 var getCoords = function(city) {
 
-    api
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+ city + "&limit=1&appid=" + apiKey
+
+    fetch(apiUrl)
+    .then(function(response){
+        if (response.ok) {
+            response.json().then(function(data) {
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+
+                getWeather(lat, lon);
+
+            })
+        }
+    })
 }
 
 // plug coords into one call api
 
+var getWeather = function(lat, lon) {
+    apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + apiKey;
 
+    fetch(apiUrl)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data)
+            })
+        }
+    })
+}
 // display results in console
 
 
