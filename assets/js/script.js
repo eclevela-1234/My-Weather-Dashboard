27026eaa0cf11d5000438324638d03c72e3d32c9
Eliot Cleveland <eclevela@richmond.edu>
2022-07-03 23:21:07 -0400

    data displays in cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ var windEl = document.querySelector("#wind");
 var humEl = document.querySelector("#humidity");
 var uvEl = document.querySelector("#uv-index");
 var weatherHeaderEl = document.querySelector("#weather-header");
+var dailyWeatherEl = document.querySelector("#daily-weather");
 
 
 //to test functionality, please disable import and add an OpenWeather api key below
@@ -54,7 +55,7 @@ var getWeather = function (lat, lon) {
     }
   });
 };
-// display results in console
+
 var displayData = function(data){
     //create span elements
     var curTemp = document.createElement("span");
@@ -77,11 +78,62 @@ var displayData = function(data){
     weatherHeaderEl.textContent += " (" + date +")"
     console.log(date);
     
+    dailyData(data);
 
 
 };
+var dailyData = function(data) {
+    for (var i=0; i < data.daily.length; i++) {
+        // create card elements
+        var cardEl = document.createElement("div");
+        var cardHeaderEl = document.createElement("div");
+        var listEl = document.createElement("ul");
+        var listHiTemp = document.createElement("li");
+        var listLowTemp = document.createElement("li");
+        var listWind = document.createElement("li");
+        var listHum = document.createElement("li");
+        var listUV = document.createElement("li");
 
-getCoords("sydney");
+        // assign BS classes/attributes
+        cardEl.classList = "card bg-secondary";
+        cardEl.setAttribute("style", "width: 13rem;");
+        cardHeaderEl.className = "card-header";
+        listEl.classList = "list-group list-group-flush py-2";
+        listHiTemp.classList = "list-group-item";
+        listLowTemp.className = "list-group-item";
+        listWind.className = "list-group-item";
+        listHum.className = "list-group-item";
+        listUV.className = "list-group-item";
+
+        //Get day of week
+        var day = new Date((data.daily[i].dt + data.timezone_offset) * 1000);
+        var options = { weekday: "short" };
+        // console.log(new Intl.DateTimeFormat('en-US', options).format(day));
+
+        
+        // put data in textcontent of elements
+        cardHeaderEl.textContent = new Intl.DateTimeFormat('en-US', options).format(day) + " " + day.toLocaleDateString();
+        listHiTemp.textContent = "High: " + data.daily[i].temp.max + " °F";
+        listLowTemp.textContent = "Low: " + data.daily[i].temp.min + " °F";
+        listWind.textContent = "Wind: " + data.daily[i].wind_speed + " mph";
+        listHum.textContent = "Humitity: " + data.daily[i].humidity + " %";
+        listUV.textContent = "UV Index: " + data.daily[i].uvi;
+
+        // append to div
+        dailyWeatherEl.appendChild(cardEl);
+        cardEl.appendChild(cardHeaderEl);
+        cardEl.appendChild(listEl);
+        listEl.appendChild(listHiTemp);
+        listEl.appendChild(listLowTemp);
+        listEl.appendChild(listWind);
+        listEl.appendChild(listHum);
+        listEl.appendChild(listUV);
+
+
+    }
+
+}
+getCoords("richmond");
 // traverse response to disired info and store in object array
 
 // dynamically create elements and display on page
